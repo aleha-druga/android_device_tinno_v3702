5c8b3e7f6f6b0951376204c828034e0b3975650b
Alexey <aleha.druga@gmail.com>
2017-08-22 14:44:05 +0500

    Update and rename cm.mk to mokee.mk

--- mokee.mk
+++ mokee.mk
@@ -1,11 +1,11 @@
 $(call inherit-product, device/bq/strike/device_strike.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := strike
-PRODUCT_NAME := cm_strike
-PRODUCT_BRAND := bq
-PRODUCT_MODEL := strike
-PRODUCT_MANUFACTURER := bq
+PRODUCT_NAME := mk_strike
+PRODUCT_BRAND := BQ
+PRODUCT_MODEL := Strike
+PRODUCT_MANUFACTURER := BQ
