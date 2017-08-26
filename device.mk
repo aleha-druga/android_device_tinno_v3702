138bffe5deed352a84646c897756074186bebd11
aleha-druga <aleha.druga@gmail.com>
2017-08-26 19:34:02 +0500

    not fix

--- device.mk
+++ device.mk
@@ -43,8 +43,7 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/bluetooth/bt_did.conf:system/etc/bluetooth/bt_did.conf \
 	$(LOCAL_PATH)/configs/bluetooth/bt_stack.conf:system/etc/bluetooth/bt_stack.conf \
 	$(LOCAL_PATH)/configs/bluetooth/bt_stack.conf.debug:system/etc/bluetooth/bt_stack.conf.debug \
-	$(LOCAL_PATH)/configs/bluetooth/bt_stack.conf.sqc:system/etc/bluetooth/bt_stack.conf.sqc \
-	$(LOCAL_PATH)/configs/permissions/platform.xml:system/etc/permissions/platform.xml
+	$(LOCAL_PATH)/configs/bluetooth/bt_stack.conf.sqc:system/etc/bluetooth/bt_stack.conf.sqc 
  
 # Keyboard layout
 PRODUCT_COPY_FILES += \
