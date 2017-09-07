5bd7932a725bf0fb3e76d2c811306036f2f1f694
aleha-druga <aleha.druga@gmail.com>
2017-09-08 01:53:10 +0500

    new sepolicy & symbols

--- libmtkshims/binary.cpp
+++ libmtkshims/binary.cpp
@@ -6,6 +6,7 @@ extern "C"
     void _ZN7android6Camera6createERKNS_2spINS_8hardware7ICameraEEE() { }
     void _ZN7android6Camera7connectEiRKNS_8String16Eii() { }
     void _ZN7android13GraphicBufferC1Ejjij() { }
+    void _ZN7android6Camera18setVideoBufferModeEi() { }
 }
 
 
