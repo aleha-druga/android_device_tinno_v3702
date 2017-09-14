1429c2658c2ad8890d416c73f19cffa0c588c0f3
aleha-druga <aleha.druga@gmail.com>
2017-09-15 00:55:20 +0500

    update patches

---
+++ patches/apply.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ../../../../frameworks/av 
+git apply -v ../../device/tinno/v3702/patches/frameworks_av.patch
+cd ../../system/core
+git apply -v ../../device/tinno/v3702/patches/system_core.patch
+cd ../..
+git apply -v ./device/tinno/v3702/patches/tinno.patch
+cd ../..
+git apply -v ./device/tinno/v3702/patches/mtp.patch
+
--- patches/end_key.sh
+++ patches/end_key.sh
@@ -1,6 +1,3 @@
-echo END key
-cp end_key.patch ../../../../key.patch
 cd ../../../..
-patch -p1 <key.patch
-rm end_key.patch
-echo END
+git apply -v ./device/tinno/v3702/patches/key.patch
+
