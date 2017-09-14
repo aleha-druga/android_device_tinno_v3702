6e78c8a6c3c5884249aba3b335c2f0895c0b9228
aleha-druga <aleha.druga@gmail.com>
2017-09-15 00:54:07 +0500

    update

--- device.mk
+++ device.mk
@@ -166,7 +166,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/agps_profiles_conf2.xml:system/etc/agps_profiles_conf2.xml \
 
-ADDITIONAL_DEFAULT_PROPERTIES += \
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.adb.secure=0 \
     ro.secure=0 \
     ro.allow.mock.location=1 \
