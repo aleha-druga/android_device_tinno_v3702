b1c5bfa0825068bc33c531ecd5e307fbfc7df430
aleha-druga <aleha.druga@gmail.com>
2017-09-17 01:54:47 +0500

    update

--- Android.mk
+++ Android.mk
@@ -1,7 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-#ifeq ($(TARGET_DEVICE),v3702)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
-
-#endif
+ifeq ($(TARGET_DEVICE),v3702)
+    include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,5 +1,4 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
 
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
